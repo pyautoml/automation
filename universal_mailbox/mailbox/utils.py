75a65f6e1e6a7bc54c12cad0c17293c09f15cb4b
pyautoml <147545771+pyautoml@users.noreply.github.com>
2023-11-01 23:02:43 +0100

    Update utils.py
    Pasted corrected code.

--- universal_mailbox/mailbox/utils.py
+++ universal_mailbox/mailbox/utils.py
@@ -6,43 +6,93 @@ __last_udate__ = "01.11.2023"
 __author__ = "https://github.com/pyautoml"
 __how_to__ = ["python -m unittest test.test_utils"]
 
-import gc
-import os
+import os 
+import gc 
 import sys
 import json
-import atexit
-import unittest
-import tracemalloc
-from utils import (
-    run_makefile,
-    absolute_path,
-)
+import inspect
+import subprocess
+from functools import wraps
+from typing import Type, Any
+#from logger import logger
 
 
-class TestUtils(unittest.TestCase):
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-    atexit.register(run_makefile, "remove", path)
-
-    def setUp(self) -> None:
-        tracemalloc.start()
-        self.example_file = absolute_path("settings.json")
-        self.expected_dict = {
-            "user": "user",
-            "password": "password",
-        }
-
-        with open(self.example_file, "w") as file:
-            file.write(json.dumps(self.expected_dict))
-
-    def tearDown(self) -> None:
-        tracemalloc.stop()
-        if os.path.exists(self.example_file):
-            try:
-                os.remove(self.example_file)
-            except:
-                pass
-
-
-if __name__ == "__main__":
-    unittest.main()
+def required_parameter(validation_type: Type) -> None:
+    """
+    Indicate what type the first variable of the function should be.
+    
+    Parameters
+    -----------
+    validation_type: (Type) Specified file type. Example: for string type enter "", for dict type enter {}. 
+    """
+    def decorator(original_method):
+        @wraps(original_method)
+        def wrapper(folder_name, *args, **kwargs):
+            if folder_name.strip() == "":
+                #logger.critical("Folder name cannot be empty.")
+                sys.exit(1)
+            
+            if not isinstance(folder_name, type(validation_type)):
+                #logger.critical(f"Invalid type for {folder_name}. Expected {type(validation_type)}, got {type(folder_name)}.")
+                sys.exit(1)
+            return original_method(folder_name, *args, **kwargs)
+        return wrapper
+    return decorator
+@required_parameter("")
+def load_json_data(file_path: str) -> json:
+    """
+    Read and load data from json if file exists.
+    Parameters
+    -----------
+    path (str): Path to a json file with settings. Example: /home/user/project/config/project_settings.json
+    """
+    if not os.path.exists(file_path):
+        new_path = absolute_path(file_path)
+        if not os.path.exists(file_path):
+            #logger.error(f"Path {path} does not exist.")
+            return None
+        else:
+            return new_path
+    try:
+        with open(file_path, 'r') as settings:
+            data = json.load(settings)
+    except Exception as e:
+        #logger.exception(f"e")
+        return None
+    return data
+def absolute_path(path: str = None) -> [str|None]:
+    """
+    Convert path to an absolute path. This can be useful if you are working on server.
+    Parameters
+    -----------
+    path (str):  Partial or complete path to a file or a folder. Example: './new/cat.png'
+    """
+    if not path:
+        return os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__)
+            )
+        )
+    else:
+        if not isinstance(path, str):
+            #logger.exception(f"Path must be str type, not {type(path)}.")
+            return None
+        if not path:
+            #logger.exception("Path cannot be empty.")
+            return None
+        return os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), path
+            )
+        )
+def free_up_memory() -> None:
+    """ Remove global values. """
+    for name in dir():
+        del globals()[name]
     gc.collect()
+def run_makefile(command: str, project_path: str) -> None:
+    try:
+        subprocess.run(["make", command], check=True, cwd=project_path)
+    except subprocess.CalledProcessError as e:
+        #logger.error(f"Error while running Makefile command: '{command}': {e}")
+        sys.exit(1)
