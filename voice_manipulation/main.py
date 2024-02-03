17a94759217a360340b8c042897a7159e73b6d96
PyAutoML <147545771+pyautoml@users.noreply.github.com>
2024-02-03 14:36:24 +0100

    Update main.py
    Added freq.

--- voice_manipulation/main.py
+++ voice_manipulation/main.py
@@ -6,10 +6,8 @@ import soundfile as sf
 import matplotlib.pyplot as plt
 from scipy.signal import butter, lfilter
 
-
 def universal_path(file: str) -> str:
     return os.path.abspath(os.path.join(os.path.dirname(__file__), file))
-
 
 def save_audio_spectrum_plot(mp3_file_path, output_image_path):
     y, sr = librosa.load(mp3_file_path)
@@ -24,77 +22,72 @@ def save_audio_spectrum_plot(mp3_file_path, output_image_path):
     plt.savefig(output_image_path)
     plt.close()
 
-
 def butter_highpass(cutoff, fs, order=5):
     nyquist = 0.5 * fs
     normal_cutoff = cutoff / nyquist
-    return butter(order, normal_cutoff, btype="high", analog=False)
-
+    b, a = butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
 
 def highpass_filter(data, cutoff, fs, order=5):
     b, a = butter_highpass(cutoff, fs, order=order)
-    return lfilter(b, a, data)
+    y = lfilter(b, a, data)
+    return y
 
-
-def apply_highpass_and_volume(
-    audio, sr, parts_from_to, highpass_params, volume_changes
-):
+def apply_highpass_and_volume(audio, sr, parts_from_to, highpass_params, volume_changes, frequency_changes):
     modified_audio = np.copy(audio)
 
-    for (start, end), highpass_cutoff, volume_change in zip(
-        parts_from_to, highpass_params, volume_changes
+    for (start, end), highpass_cutoff, volume_change, frequency_change in zip(
+        parts_from_to, highpass_params, volume_changes, frequency_changes
     ):
         start_frame = int(start * sr)
         end_frame = int(end * sr)
-        y_filtered = highpass_filter(
-            modified_audio[start_frame:end_frame], highpass_cutoff, sr
-        )
+
+        y_filtered = highpass_filter(modified_audio[start_frame:end_frame], highpass_cutoff, sr)
         modified_audio[start_frame:end_frame] = y_filtered
+
         modified_audio[start_frame:end_frame] *= 10 ** (volume_change / 20.0)
 
-    return modified_audio
+        modified_audio[start_frame:end_frame] = librosa.effects.pitch_shift(
+            modified_audio[start_frame:end_frame],
+            sr=sr,
+            n_steps=frequency_change / 100.0,
+        )
 
+    return modified_audio
 
 def modify_volume(
     audio_file,
     parts_from_to,
     highpass_cutoffs,
     volume_changes,
+    frequency_changes,
     new_file_name: str = "./audio/modified_audio.mp3",
 ):
     try:
         y, sr = librosa.load(audio_file, sr=None)
     except Exception as e:
-        print(f"{[ERROR] Missing audio file: {e}")
+        print(e)
         sys.exit(1)
-        
-    modified_audio = apply_highpass_and_volume(
-        y, sr, parts_from_to, highpass_cutoffs, volume_changes
-    )
-    
-    sf.write(universal_path(new_file_name), modified_audio, sr)
 
+    modified_audio = apply_highpass_and_volume(y, sr, parts_from_to, highpass_cutoffs, volume_changes, frequency_changes)
+
+    sf.write(universal_path(new_file_name), modified_audio, sr)
 
 def main():
     audio_file = universal_path("./audio/audio.mp3")
 
     parts_from_to = [[1, 5], [6.7, 12], [13, 20], [22, 30], [32, 40]]
-    highpass_cutoffs = [200, 10, 10, 10, 10]
+    highpass_cutoffs = [200, 500, 300, 200, 400]
     volume_changes = [0, 0, 0, 20, -5]
+    frequency_changes = [0, 800, -400, 0, 1000]
 
     modify_volume(
-        audio_file,
-        parts_from_to,
-        highpass_cutoffs,
-        volume_changes,
-        "./audio/modified_audio.mp3",
+        audio_file, parts_from_to, highpass_cutoffs, volume_changes, frequency_changes, "./audio/modified_audio.mp3"
     )
     save_audio_spectrum_plot(audio_file, "./spectrogram/audio_mel_spectrogram.png")
     save_audio_spectrum_plot(
-        universal_path("./audio/modified_audio.mp3"),
-        "./spectrogram/audio_modified_mel_spectrogram.png",
+        universal_path("./audio/modified_audio.mp3"), "./spectrogram/audio_modified_mel_spectrogram.png"
     )
-
 
 if __name__ == "__main__":
     main()
